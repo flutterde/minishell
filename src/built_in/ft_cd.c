8d4d2f48428d0178a7534195dcf438532cdcc76b
Otman Chouati <ochouati@c1r5p6.1337.ma>
2024-07-08 15:38:58 +0100

    cd_handler added

--- src/built_in/ft_cd.c
+++ src/built_in/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: ochouati <ochouati@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:53:19 by ochouati          #+#    #+#             */
-/*   Updated: 2024/07/07 19:37:30 by ochouati         ###   ########.fr       */
+/*   Updated: 2024/07/08 15:12:25 by ochouati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,27 @@ char	*ft_cd(char *arg)
 			strerror(errno)), free(dir), NULL);
 	getcwd(dir, 1999);
 	return (new = ft_strdup(dir), free(dir), new);
+}
+
+void	cd_handler(t_env *env, char *path)
+{
+	char	*oldpwd;
+	char	*new;
+	t_env	*node;
+
+	if (!env)
+		return ;
+	node = search_env(env, "PWD");
+	if (node)
+	{
+		oldpwd = ft_strdup(node->value);
+		if (!oldpwd)
+			return ;
+	}
+	new = ft_cd(path);
+	if (new)
+	{
+		ft_export(&env, ft_strjoin3("OLDPWD", "=", oldpwd));
+		ft_export(&env, ft_strjoin3("PWD", "=", new));
+	}
 }
